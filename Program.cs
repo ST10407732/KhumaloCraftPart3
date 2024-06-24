d0b4d1832e4497ecfdd588d4b2773004342c3649
ST10407732 <131349864+ST10407732@users.noreply.github.com>
2024-06-24 19:17:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using KhumaloCraftKC.Data;
+using KhumaloCraftKC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<KhumaloDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("KhumaloCraftKCContext")));
+
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication();
+
+var app = builder.Build();
+
+   
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    string email = "admin@admin.com";
+    string password = "Test123,";
+
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new IdentityUser
+        {
+            UserName = email,
+            Email = email
+        };
+
+        var result = await userManager.CreateAsync(user, password);
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, "Admin");
+
+            var appUser = new User
+            {
+                Id = user.Id,
+                UserId = int.Parse(user.Id), // Assuming you can cast the Id to an int, you might need to adjust this
+                Uname = "Admin User",
+                Email = email,
+                UserName = email
+            };
+            context.Users.Add(appUser);
+            await context.SaveChangesAsync();
+        }
+    }
+}
+
+    app.Run();
